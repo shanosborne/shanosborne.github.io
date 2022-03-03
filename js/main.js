85292d17d6f19dd7da7b1b37a4ccf70b54ed5c6c
Shannon Osborne <sosborne@stsci.edu>
2022-03-02 20:33:57 -0500

    clean up formatting and add links to navbar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+// Add scroll effect to header
 $(document).ready(function(){
     $('.header').height($(window).height());
     
    })
+
+// Add button scroll to navbar
+$(".navbar a").click(function(){
+    $("html, body").animate({
+     scrollTop:$("#" + $(this).data("value")).offset().top
+    },1000)
+    
+    })
